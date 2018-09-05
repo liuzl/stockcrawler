5be846ef3000057f4db610a88511154b5be13abd
Zhanliang Liu <zhanliangliu@qq.com>
2018-09-05 15:36:04 +0800

    update finance.sina.py

--- finance.sina.py
+++ finance.sina.py
@@ -3,29 +3,41 @@ import lxml
 import lxml.html
 import pandas as pd
 
-def get_data(stock, year, jidu):
-    #url = "http://money.finance.sina.com.cn/corp/go.php/vMS_MarketHistory/stockid/601006.phtml?year=2018&jidu=1"
-    url = "http://money.finance.sina.com.cn/corp/go.php/vMS_MarketHistory/stockid/%d.phtml?year=%d&jidu=%d" % (stock, year, jidu)
-    resp = requests.get(url)
-    doc = lxml.html.document_fromstring(resp.content.decode('gbk'))
-    trs = doc.xpath('//*[@id="FundHoldSharesTable"]//tr')
-    i = 0
+def get_data(stock):
+    years = [2017, 2018]
+    jidus = [1, 2, 3, 4]
     header = None
     data = []
-    for tr in trs[1:]:
-        tds = tr.xpath(".//td")
-        row = []
-        for td in tds:
-            text = lxml.etree.tounicode(td, method='text').strip()
-            row.append(text)
-        if i == 0:
-            header = row
-        else:
-            data.append(row)
-        i += 1
+    for year in years:
+        for jidu in jidus:
+            url = "http://money.finance.sina.com.cn/corp/go.php/vMS_MarketHistory/stockid/%d.phtml?year=%d&jidu=%d" % (stock, year, jidu)
+            print("download %s" % url)
+            resp = requests.get(url)
+            doc = lxml.html.document_fromstring(resp.content.decode('gbk'))
+            trs = doc.xpath('//*[@id="FundHoldSharesTable"]//tr')
+            i = 0
+            for tr in trs[1:]:
+                tds = tr.xpath(".//td")
+                row = []
+                for td in tds:
+                    text = lxml.etree.tounicode(td, method='text').strip()
+                    row.append(text)
+                if i == 0:
+                    header = row
+                else:
+                    data.append(row)
+                i += 1
     df = pd.DataFrame(data=data, columns=header)
-    df.to_csv("%d_%d_%d.csv" % (stock, year, jidu), index=False)
+    filename = "%d.csv" % stock
+    print("saving to %s" % filename)
+    df.to_csv(filename, index=False)
     return df
 
 if __name__ == "__main__":
-    get_data(601006, 2018, 1)
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python %s <stock_id>" % sys.argv[0])
+        sys.exit(1)
+    stock = int(sys.argv[1])
+    get_data(stock)
+    #get_data(601006)
